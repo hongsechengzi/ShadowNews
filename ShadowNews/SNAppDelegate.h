f5d12d3b8e3111b4fc8a7e47b34095272ec17605
hongsechengzi <lanou3g@lianbinjing-2.local>
2014-06-28 10:31:00 +0800

    Initial Commit

---
+++ ShadowNews/SNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SNAppDelegate.h
+//  ShadowNews
+//
+//  Created by lanou3g on 14-6-28.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShadowNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShadowNews
+//
+//  Created by lanou3g on 14-6-28.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SNAppDelegate class]));
+    }
+}
